8beead16665fccbda5307c074f5c328596d59cfb
hilmab2005-cpu <hilm.ab2005@gmail.com>
2026-04-09 15:08:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,37 +2,50 @@
  * Selects the element with the id 'main-heading' and changes its text content to 'DOM Manipulation Challenge'.
  */
 function changeHeadingText() {
-  // TODO: Implement this function
+  const heading = document.getElementById('main-heading');
+  heading.textContent = 'DOM Manipulation Challenge';      
 }
+changeHeadingText();
+  // TODO: Implement this function
 
 /**
  * Selects the element with the id 'box-to-modify' and changes its background color to 'lightblue'.
  */
 function changeBoxColor() {
+    const box = document.getElementById('box-to-modify');
+    box.style.backgroundColor= 'lightblue';
   // TODO: Implement this function
 }
-
+changeBoxColor(); 
 /**
  * Creates a new <li> element, sets its text content to 'New Item', and appends it to the <ul> with the id 'item-list'.
  */
 function addNewItem() {
+    const newitem= document.createElement('li');
+    newitem.textContent='New Item';
+    const itemlist= document.getElementById('item-list');
+    itemlist.appendChild(newitem);
   // TODO: Implement this function
 }
-
+addNewItem(); 
 /**
  * Selects the paragraph with the class 'content-para' and adds the class 'highlight' to it.
  */
 function highlightParagraph() {
-  // TODO: Implement this function
+    const para = document.querySelector('.content-para'); 
+  para.classList.add('highlight');                      
 }
-
+  // TODO: Implement this function
+highlightParagraph();
 /**
  * Selects the element with the id 'to-be-removed' and removes it from the DOM.
  */
 function removeElement() {
+    const remove= document.getElementById('to-be-removed');
+    remove.remove();
   // TODO: Implement this function
 }
-
+removeElement();  
 
 // Do not edit the lines below.
 // These lines are for testing purposes.
@@ -45,4 +58,3 @@ if (typeof module !== 'undefined' && module.exports) {
         removeElement
     };
 }
-
